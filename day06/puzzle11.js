e65c675959f8a91c6094a52536212ba36beda09f
jnsmtnr <janosmaitner@gmai.com>
2025-11-17 13:47:22 +0100

    day 6 finished

---
+++ day06/puzzle11.js
@@ -0,0 +1,23 @@
+import fs from "fs";
+const input = fs.readFileSync("input.txt", "utf-8");
+
+const letters = [];
+
+for (const line of input.split("\n")) {
+  if (!line) break;
+
+  for (const index in line) {
+    const letter = line[index];
+    if (!letters[index]) letters.push({});
+
+    if (!letters[index][letter]) letters[index][letter] = 0;
+
+    letters[index][letter]++;
+  }
+
+  console.log(
+    letters
+      .map((index) => Object.entries(index).sort((a, b) => b[1] - a[1])[0][0])
+      .join("")
+  );
+}
